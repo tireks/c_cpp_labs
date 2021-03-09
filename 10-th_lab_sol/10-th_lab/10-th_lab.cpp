d100b82b51504a7f5b7cb2c2383ffcb64a84dd5b
tireks <leonru007@gmail.com>
2021-03-10 01:49:31 +0700

    full working solution

--- 10-th_lab_sol/10-th_lab/10-th_lab.cpp
+++ 10-th_lab_sol/10-th_lab/10-th_lab.cpp
@@ -2,10 +2,58 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    short amount_mass;
+    short container_mass[18];
+    cout << "input amount of nums in array" << endl;
+    cin >> amount_mass;
+    for (short i = 0; i < 18; i++)
+    {
+        container_mass[i] = 0;
+    }
+    if ((amount_mass < 0) and (amount_mass > 1000))
+    {
+        return 0;
+    }
+    short* dyn_arr_input;
+    dyn_arr_input = new short[amount_mass];
+    short* dyn_arr_output;
+    dyn_arr_output = new short[amount_mass];
+    int counter = 0;
+    cout << "now input as many numbers as you typed" << endl;
+    for (short i = 0; i < amount_mass; i++)
+    {
+        cin >> *(dyn_arr_input + i) ;
+        if ((*(dyn_arr_input + i) > 17) and (*(dyn_arr_input + i) < 0))
+        {
+            return 0;
+        }
+        container_mass[*(dyn_arr_input + i)]++;
+    }
+    cout << "results of 'garmoshka':" << endl;
+    for (short i = 0; i < 18; i++)
+    {
+        cout << i << ": " << container_mass[i] << endl;
+    }
+    cout << "now, unzipped array" << endl;
+    for (short i = 0; i < amount_mass; i++)
+    {
+        while (container_mass[counter] != 0)
+        {
+            *(dyn_arr_output + i) = counter;
+            cout << *(dyn_arr_output + i) << "  ";
+            container_mass[counter]--;
+        }
+        counter++;
+    }
+
+    delete[] dyn_arr_input;
+    delete[] dyn_arr_output;
+
+
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
