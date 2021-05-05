0aa77b065ae7916d64dba687856f744781ee99d0
tireks <leonru007@gmail.com>
2021-05-05 16:03:39 +0700

    made simple input and output of matrix

---
+++ 13_lab_1-task/13_lab_1-task/13_lab_1-task.cpp
@@ -0,0 +1,39 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int** matrix_arr;
+    int num_strings;
+    int i_count;
+    int j_count;
+    int num_columns;
+    cout << "input num of strs" << endl;
+    cin >>num_strings;
+    cout << "input num of cols" << endl;
+    cin >> num_columns;
+    matrix_arr = new int*[num_strings];
+    for (i_count = 0; i_count < num_strings; i_count++)
+    {
+        matrix_arr[i_count] = new int[num_columns];
+    }
+    for (i_count = 0; i_count < num_strings; i_count++)
+    {
+        for (j_count = 0; j_count < num_columns; j_count++)
+        {
+            cin >> matrix_arr[i_count][j_count];
+        }
+    }
+
+    for (i_count = 0; i_count < num_strings; i_count++)
+    {
+        cout << endl;
+        for (j_count = 0; j_count < num_columns; j_count++)
+        {
+            cout << matrix_arr[i_count][j_count] << "  ";
+        }
+    }
+}
+
