5d867e7df51efb48b7f38aec3d40c9b221e9832b
tireks <leonru007@gmail.com>
2021-05-06 14:00:35 +0700

    made reading from file and switched settings of proj

--- 13_lab_1-task/13_lab_1-task/13_lab_1-task.cpp
+++ 13_lab_1-task/13_lab_1-task/13_lab_1-task.cpp
@@ -10,20 +10,32 @@ int main()
     int i_count;
     int j_count;
     int num_columns;
-    cout << "input num of strs" << endl;
+    FILE* file_input;
+    char name[] = "test1.txt";
+    /*cout << "input num of strs" << endl;
     cin >>num_strings;
     cout << "input num of cols" << endl;
-    cin >> num_columns;
+    cin >> num_columns;*/
+    if ((file_input = fopen(name, "r")) == NULL)
+    {
+        printf("Не удалось открыть файл");
+        getchar();
+        return 0;
+    }
+    fscanf(file_input, "%d", &num_strings);
+    fscanf(file_input, "%d", &num_columns);
     matrix_arr = new int*[num_strings];
     for (i_count = 0; i_count < num_strings; i_count++)
     {
         matrix_arr[i_count] = new int[num_columns];
     }
+    
     for (i_count = 0; i_count < num_strings; i_count++)
     {
         for (j_count = 0; j_count < num_columns; j_count++)
         {
-            cin >> matrix_arr[i_count][j_count];
+            //cin >> matrix_arr[i_count][j_count];
+            fscanf(file_input, "%d", &matrix_arr[i_count][j_count]);
         }
     }
 
@@ -35,5 +47,6 @@ int main()
             cout << matrix_arr[i_count][j_count] << "  ";
         }
     }
+    free(matrix_arr);
 }
 
