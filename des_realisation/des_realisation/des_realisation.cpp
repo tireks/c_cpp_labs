55b5686f4fec8c20d8006654b14d12bb8edd1d64
tireks <leonru007@gmail.com>
2021-05-12 13:55:06 +0700

    first drafts of code

---
+++ des_realisation/des_realisation/des_realisation.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int tab_start_recomb(int num_of_bit) {
+    int mass_table[64] =
+    {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
+     62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
+     57, 49, 41, 33, 25, 17, 9,  1, 59, 51, 43, 35, 27, 19, 11, 3,
+     61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
+
+}
+
+int main()
+{
+    __int64 start_string;
+    __int64 end_string;
+    cin >> start_string;
+    
+}
+
+
