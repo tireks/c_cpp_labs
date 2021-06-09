9ad7af3e6274c899c0765d9ff3073afe5652b3b7
tireks <leonru007@gmail.com>
2021-06-09 16:58:19 +0700

    --

--- file_manager/file_manager/file_manager.cpp
+++ file_manager/file_manager/file_manager.cpp
@@ -3,12 +3,32 @@
 #include <iostream>
 using namespace std;
 
-typedef struct Node {
+typedef struct Node_1 {
     int user_id;
     char login[11];
     char password[9];
-    struct Node* next;
+    struct Node_1* next;
 } Node_auth;
+
+typedef struct Node_2 {
+    int user_id;
+    bool read_permition;
+    bool write_permition;
+    bool append_permition;
+    struct Node_2* next;
+} Node_Pmatr_inner;
+
+typedef struct Node_3 {
+    int file_id;
+    struct Node_3* next;
+    Node_Pmatr_inner* containment;
+} Node_Pmatr_outter;
+
+typedef struct Node_4 {
+    int file_id;
+    char filename[16];
+    struct Node_4* next;
+} Node_file_id_comp;
 
 Node_auth* create_auth(int id_input, char* login_, char* pass)
 {
@@ -16,6 +36,35 @@ Node_auth* create_auth(int id_input, char* login_, char* pass)
     tmp->user_id = id_input;
     memcpy(tmp->login, login_, 11);
     memcpy(tmp->password, pass, 9);
+    tmp->next = NULL;
+    return(tmp);
+}
+
+Node_file_id_comp* create_comp_tab_file(int file_id_, char* filename_)
+{
+    Node_file_id_comp* tmp = (Node_file_id_comp*)malloc(sizeof(Node_file_id_comp));
+    tmp->file_id = file_id_;
+    memcpy(tmp->filename, filename_, 16);
+    tmp->next = NULL;
+    return(tmp);
+}
+
+Node_Pmatr_inner* create_Pmatr_inner(int user_id_, bool r_p, bool w_p, bool a_p)
+{
+    Node_Pmatr_inner* tmp = (Node_Pmatr_inner*)malloc(sizeof(Node_Pmatr_inner));
+    tmp->user_id = user_id_;
+    tmp->read_permition = r_p;
+    tmp->write_permition = w_p;
+    tmp->append_permition = a_p;
+    tmp->next = NULL;
+    return(tmp);
+}
+
+Node_Pmatr_outter* create_Pmatr_outter(Node_Pmatr_inner* start_cont, int file_id_)
+{
+    Node_Pmatr_outter* tmp = (Node_Pmatr_outter*)malloc(sizeof(Node_Pmatr_outter));
+    tmp->file_id = file_id_;
+    tmp->containment = start_cont;
     tmp->next = NULL;
     return(tmp);
 }
@@ -53,20 +102,88 @@ void add_element_auth(Node_auth* list, int id_input, char* login_, char* pass)
     p->next = tmp;
 }
 
+void add_element_file_tab(Node_file_id_comp* list, int file_id_, char* filename_)
+{
+    Node_file_id_comp* tmp = (Node_file_id_comp*)malloc(sizeof(Node_file_id_comp));
+    memcpy(tmp->filename, filename_, 16);
+    tmp->file_id = file_id_;
+    tmp->next = NULL;
+    Node_file_id_comp* p = list;
+    while (p->next != NULL)
+        p = p->next;
+    p->next = tmp;
+}
+
+void add_element_Pmatr_inner(Node_Pmatr_inner* list, int user_id_, bool r_p, bool w_p, bool a_p)
+{
+    Node_Pmatr_inner* tmp = (Node_Pmatr_inner*)malloc(sizeof(Node_Pmatr_inner));
+    tmp->user_id = user_id_;
+    tmp->read_permition = r_p;
+    tmp->write_permition = w_p;
+    tmp->append_permition = a_p;
+    tmp->next = NULL;
+    Node_Pmatr_inner* p = list;
+    while (p->next != NULL)
+        p = p->next;
+    p->next = tmp;
+}
+
+void add_element_Pmatr_outter(Node_Pmatr_outter* list, Node_Pmatr_inner* cont, int file_id_)
+{
+    Node_Pmatr_outter* tmp = (Node_Pmatr_outter*)malloc(sizeof(Node_Pmatr_outter));
+    tmp->file_id = file_id_;
+    tmp->containment = cont;
+    tmp->next = NULL;
+    Node_Pmatr_outter* p = list;
+    while (p->next != NULL)
+        p = p->next;
+    p->next = tmp;
+}
+
 Node_auth* remove_all(Node_auth* head)
 {
-    // Сдвиг указателя head в самый конец первоначального списка
     while (head != NULL)
     {
-        // Присваивание новому указателю указателя head
         Node_auth* p = head;
         head = head->next;
-        // Освобождение памяти для указателя p
         free(p);
     }
     return NULL;
 }
 
+Node_file_id_comp* remove_all_file_comp(Node_file_id_comp* head)
+{
+    while (head != NULL)
+    {
+        Node_file_id_comp* p = head;
+        head = head->next;
+        free(p);
+    }
+    return NULL;
+}
+
+Node_Pmatr_inner* remove_all_Pmatr_inner(Node_Pmatr_inner* head)
+{
+    while (head != NULL)
+    {
+        Node_Pmatr_inner* p = head;
+        head = head->next;
+        free(p);
+    }
+    return NULL;
+}
+
+Node_Pmatr_outter* remove_all_Pmatr_outter(Node_Pmatr_outter* head)
+{
+    while (head != NULL)
+    {
+        Node_Pmatr_outter* p = head;
+        remove_all_Pmatr_inner(head->containment);
+        head = head->next;
+        free(p);
+    }
+    return NULL;
+}
 
 bool login_check(char* login_, Node_auth* list)
 {
@@ -91,7 +208,7 @@ bool login_check(char* login_, Node_auth* list)
     }
 }
 
-bool password_check(char* password, Node_auth* list)
+bool password_check(char* password, Node_auth* list, int* id_sess)
 {
     Node_auth* p;
     p = list;
@@ -101,6 +218,10 @@ bool password_check(char* password, Node_auth* list)
         if (strlen(p->password) == strlen(password))
         {
             authoriser = strcmp(p->password, password);
+            if (authoriser == 0)
+            {
+                *id_sess = p->user_id;
+            }
         }
         
         p = p->next; // переход к следующему узлу
@@ -115,7 +236,8 @@ bool password_check(char* password, Node_auth* list)
     }
 }
 
-void create_new_user(char* login_, char* pass_, Node_auth* list)
+void create_new_user(char* login_, char* pass_, Node_auth* list, 
+                    Node_Pmatr_outter* per_matr, int* id_sess)
 {
     Node_auth* p;
     p = list;
@@ -126,6 +248,80 @@ void create_new_user(char* login_, char* pass_, Node_auth* list)
         p = p->next; // переход к следующему узлу
     } while (p != NULL);
     add_element_auth(list, temp_counter + 1, login_, pass_);
+    Node_Pmatr_outter* out_curs;
+    out_curs = per_matr;
+    //int temp_counter = 0;
+    do {
+        add_element_Pmatr_inner(out_curs->containment, temp_counter + 1, 1, 0, 0);
+        out_curs = out_curs->next; // переход к следующему узлу
+    } while (out_curs != NULL);
+    *id_sess = temp_counter + 1;
+}
+
+int compare_filename_id(char* filename_, Node_file_id_comp* list)
+{
+    Node_file_id_comp* p;
+    p = list;
+    int temp_ = -1;
+    do {
+        //printf("%d ", p->field); // вывод значения элемента p
+        if (strlen(p->filename) == strlen(filename_))
+        {
+            temp_ = strcmp(p->filename, filename_);
+        }
+        if (temp_ != 0)
+        {
+            p = p->next; // переход к следующему узлу
+        }
+    } while ((p != NULL) && (temp_ != 0));
+    if (temp_ != 0)
+    {
+        return -1;
+    }
+    if (temp_ == 0)
+    {
+        return p->file_id;
+    }
+}
+
+int add_file_per_matr(Node_Pmatr_outter* per_matr, int* id_sess, int file_id)
+{
+    if (file_id == -1)
+    {
+        return -1;
+    }
+    add_element_Pmatr_outter(per_matr, create_Pmatr_inner(1,1,1,1), file_id);
+    Node_Pmatr_outter* p;
+    Node_Pmatr_outter* z;
+    p = per_matr;
+    z = p;
+    int temp_counter = 1;
+    do {
+        //printf("%d ", p->field); // вывод значения элемента p
+        z = p;
+        p = p->next;
+        temp_counter++;
+    } while (p->next != NULL);
+    if ((temp_counter) != file_id)
+    {
+        return -2;
+    }
+    Node_Pmatr_inner* t;
+    Node_Pmatr_inner* s;
+    t = z->next->containment;
+    s = p->containment;
+    do {
+        if (t->user_id == *id_sess)
+        {
+            add_element_Pmatr_inner(s, t->user_id, 1, 1, 1);
+        }
+        else
+        {
+            add_element_Pmatr_inner(s, t->user_id, 1, 0, 0);
+        }
+        t = t->next;
+    } while (t != NULL);
+    return 1;
 }
 
 int main()
@@ -134,6 +330,8 @@ int main()
     bool kill_session_flag = false;
     bool sess_authorised = false;
     int temp_action_switch = 0;
+    int cur_id_sess;
+    cur_id_sess = 0;
     char* login_ = new char[11];
     
     /*for (size_t i = 0; i < 11; i++)
@@ -158,6 +356,15 @@ int main()
     password[4] = (char)('n');
     password[5] = 0;
     Node_auth* auth_data_list = create_auth(1, login_, password);
+    char* filename = new char[16];
+    password[0] = (char)('f');
+    password[1] = (char)('i');
+    password[2] = (char)('l');
+    password[3] = (char)('e');
+    password[4] = (char)('1');
+    password[5] = 0;
+    Node_file_id_comp* file_comp_tab = create_comp_tab_file(1, filename);
+    Node_Pmatr_outter* permition_matr = create_Pmatr_outter(create_Pmatr_inner(1, 1, 1, 1), 1);
     while (!kill_session_flag)
     {
         while (!sess_authorised)
@@ -188,7 +395,7 @@ int main()
                         {
                             cout << "login found, password:" << endl;
                             cin >> password;
-                            pass_correct = password_check(password, auth_data_list);
+                            pass_correct = password_check(password, auth_data_list, &cur_id_sess);
                             if (pass_correct)
                             {
                                 sess_authorised = true;
@@ -212,13 +419,13 @@ int main()
                     cin >> login_;
                     cout << "input password, which you'll use to operate(max - 8 symbols)" << endl;
                     cin >> password;
-                    create_new_user(login_, password, auth_data_list);
+                    create_new_user(login_, password, auth_data_list, permition_matr, &cur_id_sess);
                     cout << "Perfect, data saved, you're authorised now" << endl;
                     sess_authorised = true;
                     ///////
                     Node_auth* p;
                     p = auth_data_list;
-                    int temp_counter = 0;
+                    //int temp_counter = 0;
                     do {
                         //printf("%d ", p->field); // вывод значения элемента p
                         cout << "login:" << p->login << endl;
@@ -227,12 +434,39 @@ int main()
                     } while (p != NULL);
                     ///////
                     break;
+                case 3:
+                    remove_all(auth_data_list);
+                    remove_all_file_comp(file_comp_tab);
+                    remove_all_Pmatr_outter(permition_matr);
+                    free(login_);
+                    free(password);
+                    return 0;
+                    break;
                 }
             }
+        }
+        while (sess_authorised)
+        {
+            cout << "testy functional" << endl;
+            cout << "type name of file" << endl;
+            cin >> filename;
+            int i_c = 1;
+            Node_file_id_comp* temp_curs;
+            temp_curs = file_comp_tab;
+            do 
+            {   
+                i_c++;
+                temp_curs = temp_curs->next; // переход к следующему узлу
+            } while (temp_curs != NULL);
+            add_element_file_tab(file_comp_tab, i_c, filename);
+            //написать компэрер названия файла и id по табл, будет возвращать id, нужно будет для универсального добавления прав в матрицу, затем само добавление и вывод матрицы
+ 
         }
         kill_session_flag = true; //////temp!!!!!
     }
     remove_all(auth_data_list);
+    remove_all_file_comp(file_comp_tab);
+    remove_all_Pmatr_outter(permition_matr);
     free(login_);
     free(password);
 }
