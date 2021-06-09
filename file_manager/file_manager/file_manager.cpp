30e55b8ec5dbaec3ff51bb88688fc4df65b1413b
tireks <leonru007@gmail.com>
2021-06-10 05:48:35 +0700

    works well, but without real file-working

--- file_manager/file_manager/file_manager.cpp
+++ file_manager/file_manager/file_manager.cpp
@@ -284,6 +284,18 @@ int compare_filename_id(char* filename_, Node_file_id_comp* list)
     }
 }
 
+void distr_content_wr(Node_file_id_comp* list)
+{
+    cout << "//////////////////////////////////////////////////////////" << endl;
+    Node_file_id_comp* p;
+    p = list;
+    do {
+        cout << p->filename << ".txt" << endl;
+        p = p->next; // переход к следующему узлу
+    } while (p != NULL);
+    cout << "//////////////////////////////////////////////////////////" << endl;
+}
+
 int add_file_per_matr(Node_Pmatr_outter* per_matr, int* id_sess, int file_id)
 {
     if (file_id == -1)
@@ -308,20 +320,131 @@ int add_file_per_matr(Node_Pmatr_outter* per_matr, int* id_sess, int file_id)
     }
     Node_Pmatr_inner* t;
     Node_Pmatr_inner* s;
-    t = z->next->containment;
+    t = z->containment->next;
     s = p->containment;
     do {
-        if (t->user_id == *id_sess)
+        if (*id_sess != 1)
         {
-            add_element_Pmatr_inner(s, t->user_id, 1, 1, 1);
+            if (t->user_id == *id_sess)
+            {
+                add_element_Pmatr_inner(s, t->user_id, 1, 1, 1);
+            }
+            else
+            {
+                add_element_Pmatr_inner(s, t->user_id, 1, 0, 0);
+            }
+            t = t->next;
+        }
+    } while (t != NULL);
+    return 1;
+}
+
+bool check_perm_file(int file_id, int* user_id, int perm_lvl, Node_Pmatr_outter* list)
+{
+    Node_Pmatr_outter* p;
+    p = list;
+    do {
+        if (p->file_id != file_id)
+        {
+            p = p->next;
         }
         else
         {
-            add_element_Pmatr_inner(s, t->user_id, 1, 0, 0);
+            Node_Pmatr_inner* s;
+            s = p->containment;
+            do
+            {
+                if (s->user_id != *user_id)
+                {
+                    s = s->next;
+                }
+                else
+                {
+                    switch (perm_lvl)
+                    {
+                    case 1:
+                        if (s->read_permition)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    case 2:
+                        if (s->write_permition)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    case 3:
+                        if (s->append_permition)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    }
+                }
+            } while (s != NULL);
+            p = NULL;
         }
-        t = t->next;
-    } while (t != NULL);
-    return 1;
+    } while (p != NULL);
+}
+
+void permission_changer(Node_Pmatr_outter* list, int user_id, int new_perm, int file_id)
+{
+    Node_Pmatr_outter* p;
+    p = list;
+    do
+    {
+        if (p->file_id != file_id)
+        {
+            p = p->next;
+        }
+        else
+        {
+            Node_Pmatr_inner* s;
+            s = p->containment;
+            do
+            {
+                if (s->user_id != user_id)
+                {
+                    s = s->next;
+                }
+                else
+                {
+                    switch (new_perm)
+                    {
+                    case 1:
+                        s->read_permition = true;
+                        s->write_permition = false;
+                        s->append_permition = false;
+                        break;
+                    case 2:
+                        s->read_permition = true;
+                        s->write_permition = true;
+                        s->append_permition = false;
+                    case 3:
+                        s->read_permition = true;
+                        s->write_permition = true;
+                        s->append_permition = true;
+                        break;
+                    }
+                    s = NULL;
+                }
+            } while (s != NULL);
+            p = NULL;
+        }
+    } while (p != NULL);
 }
 
 int main()
@@ -331,7 +454,16 @@ int main()
     bool sess_authorised = false;
     int temp_action_switch = 0;
     int cur_id_sess;
+    int iter__;
+    int i_c;
+    Node_file_id_comp* p_file;
+    Node_file_id_comp* temp_curs;
+    Node_file_id_comp* temp_curs1;
+    Node_Pmatr_outter* temp_curs2;
+    Node_auth* p_auth_k;
     cur_id_sess = 0;
+    bool perm_granted;
+    bool stop_cycl;
     char* login_ = new char[11];
     
     /*for (size_t i = 0; i < 11; i++)
@@ -357,12 +489,12 @@ int main()
     password[5] = 0;
     Node_auth* auth_data_list = create_auth(1, login_, password);
     char* filename = new char[16];
-    password[0] = (char)('f');
-    password[1] = (char)('i');
-    password[2] = (char)('l');
-    password[3] = (char)('e');
-    password[4] = (char)('1');
-    password[5] = 0;
+    filename[0] = (char)('f');
+    filename[1] = (char)('i');
+    filename[2] = (char)('l');
+    filename[3] = (char)('e');
+    filename[4] = (char)('1');
+    filename[5] = 0;
     Node_file_id_comp* file_comp_tab = create_comp_tab_file(1, filename);
     Node_Pmatr_outter* permition_matr = create_Pmatr_outter(create_Pmatr_inner(1, 1, 1, 1), 1);
     while (!kill_session_flag)
@@ -438,6 +570,7 @@ int main()
                     remove_all(auth_data_list);
                     remove_all_file_comp(file_comp_tab);
                     remove_all_Pmatr_outter(permition_matr);
+                    free(filename);
                     free(login_);
                     free(password);
                     return 0;
@@ -447,26 +580,281 @@ int main()
         }
         while (sess_authorised)
         {
-            cout << "testy functional" << endl;
-            cout << "type name of file" << endl;
-            cin >> filename;
-            int i_c = 1;
-            Node_file_id_comp* temp_curs;
-            temp_curs = file_comp_tab;
-            do 
-            {   
-                i_c++;
-                temp_curs = temp_curs->next; // переход к следующему узлу
-            } while (temp_curs != NULL);
-            add_element_file_tab(file_comp_tab, i_c, filename);
-            //написать компэрер названия файла и id по табл, будет возвращать id, нужно будет для универсального добавления прав в матрицу, затем само добавление и вывод матрицы
- 
+            distr_content_wr(file_comp_tab);
+            cout << "if you wanna know about available commands - type 0" << endl;
+            cout << "or type command that you know" << endl;
+            cin >> temp_action_switch;
+            switch (temp_action_switch)
+            {
+            case 0:
+                cout << "list of commands:" << endl;
+                cout << "1-open one of files for reading" << endl;
+                cout << "2-open one of files for reading and writing" << endl;
+                cout << "3-check append permission for one of files" << endl;
+                cout << "4-create file with standart permissions" << endl;
+                cout << "5-create file with settable permissions" << endl;
+                cout << "6-chahge permission for my appendable files" << endl;
+                cout << "9-logout " << endl;
+                break;
+            case 1:
+                cout << "which file?" << endl;
+                iter__ = 0;
+                p_file = file_comp_tab;
+                do {
+                    iter__++;
+                    cout << iter__<< " - " << p_file->filename << ".txt" << endl;
+                    p_file = p_file->next; // переход к следующему узлу
+                } while (p_file != NULL);
+                cin >> iter__;
+                temp_curs1 = file_comp_tab;
+                temp_curs2 = permition_matr;
+                do
+                {
+                    if (iter__ != temp_curs1->file_id)
+                    {
+                        temp_curs2 = temp_curs2->next;
+                        temp_curs1 = temp_curs1->next;
+                    }
+                    else
+                    {
+                        memcpy(filename, temp_curs1->filename, 16);
+                        perm_granted = check_perm_file(iter__, &cur_id_sess, 1, permition_matr);
+                        if (perm_granted)
+                        {
+                            cout << "Success!" << endl;
+                            //////////// -- место для работы с файлом
+                        }
+                        else
+                        {
+                            cout << "Sorry, you dont have permissions" << endl;
+                        }
+                        temp_curs1 = NULL;
+                    }
+                     
+                } while ((temp_curs1 != NULL) && (temp_curs2 != NULL));
+                break;
+            case 2:
+                cout << "which file?" << endl;
+                iter__ = 0;
+                p_file = file_comp_tab;
+                do {
+                    iter__++;
+                    cout << iter__ << " - " << p_file->filename << ".txt" << endl;
+                    p_file = p_file->next; // переход к следующему узлу
+                } while (p_file != NULL);
+                cin >> iter__;
+                temp_curs1 = file_comp_tab;
+                temp_curs2 = permition_matr;
+                do
+                {
+                    if (iter__ != temp_curs1->file_id)
+                    {
+                        temp_curs2 = temp_curs2->next;
+                        temp_curs1 = temp_curs1->next;
+                    }
+                    else
+                    {
+                        memcpy(filename, temp_curs1->filename, 16);
+                        perm_granted = check_perm_file(iter__, &cur_id_sess, 2, permition_matr);
+                        if (perm_granted)
+                        {
+                            cout << "Success!" << endl;
+                            //////////// -- место для работы с файлом
+                        }
+                        else
+                        {
+                            cout << "Sorry, you dont have permissions" << endl;
+                        }
+                        temp_curs1 = NULL;
+                    }
+
+                } while ((temp_curs1 != NULL) && (temp_curs2 != NULL));
+                break;
+            case 3:
+                cout << "which file?" << endl;
+                iter__ = 0;
+                p_file = file_comp_tab;
+                do {
+                    iter__++;
+                    cout << iter__ << " - " << p_file->filename << ".txt" << endl;
+                    p_file = p_file->next;
+                } while (p_file != NULL);
+                cin >> iter__;
+                temp_curs1 = file_comp_tab;
+                temp_curs2 = permition_matr;
+                do
+                {
+                    if (iter__ != temp_curs1->file_id)
+                    {
+                        temp_curs2 = temp_curs2->next;
+                        temp_curs1 = temp_curs1->next;
+                    }
+                    else
+                    {
+                        memcpy(filename, temp_curs1->filename, 16);
+                        perm_granted = check_perm_file(iter__, &cur_id_sess, 3, permition_matr);
+                        if (perm_granted)
+                        {
+                            cout << "Success! You have admin permissions for that file" << endl;
+                            ///////// -- место для какихто действий
+                        }
+                        else
+                        {
+                            cout << "Sorry, you dont have permissions" << endl;
+                        }
+                        temp_curs1 = NULL;
+                    }
+
+                } while ((temp_curs1 != NULL) && (temp_curs2 != NULL));
+                break;
+            case 4:
+                cout << "type name of file" << endl;
+                cin >> filename;
+                i_c = 1;
+                temp_curs = file_comp_tab;
+                do
+                {
+                    i_c++;
+                    temp_curs = temp_curs->next; // переход к следующему узлу
+                } while (temp_curs != NULL);
+                add_element_file_tab(file_comp_tab, i_c, filename);
+                //написать компэрер названия файла и id по табл, будет возвращать id, нужно будет для универсального добавления прав в матрицу, затем само добавление и вывод матрицы
+                i_c = add_file_per_matr(permition_matr, &cur_id_sess, compare_filename_id(filename, file_comp_tab));
+                switch (i_c)
+                {
+                case -1:
+                    cout << "some problems with compare table" << endl;
+                    break;
+                case -2:
+                    cout << "some problems with comparing in table or in cycle" << endl;
+                    break;
+                case 1:
+                    cout << "everything's great" << endl;
+                    break;
+                }
+                break;
+            case 5:
+                stop_cycl = false;
+                cout << "type name of file" << endl;
+                cin >> filename;
+                i_c = 1;
+                temp_curs = file_comp_tab;
+                do
+                {
+                    i_c++;
+                    temp_curs = temp_curs->next; // переход к следующему узлу
+                } while (temp_curs != NULL);
+                add_element_file_tab(file_comp_tab, i_c, filename);
+                i_c = add_file_per_matr(permition_matr, &cur_id_sess, compare_filename_id(filename, file_comp_tab));
+                switch (i_c)
+                {
+                case -1:
+                    cout << "some problems with compare table" << endl;
+                    break;
+                case -2:
+                    cout << "some problems with comparing in table or in cycle" << endl;
+                    break;
+                case 1:
+                    cout << "everything's great" << endl;
+                    break;
+                }
+                while (!stop_cycl)
+                {
+                    int iter__ = 0;
+                    int iter1__ = 0;
+                    p_auth_k = auth_data_list;
+                    do {
+                        iter__++;
+                        cout << iter__ << " - " << p_auth_k->login << endl;
+                        p_auth_k = p_auth_k->next;
+                    } while (p_auth_k != NULL);
+                    cout <<  "which user?" << endl;
+                    cin >> iter__;
+                    if (iter__ != 1)
+                    {
+                        cout << "which permission want to grant?" << endl;
+                        cout << "1 - read" << endl;
+                        cout << "2 - read and write" << endl;
+                        cout << "3 - append permission" << endl;
+                        cin >> iter1__;
+                        permission_changer(permition_matr, iter__, iter1__, compare_filename_id(filename, file_comp_tab));
+                        cout << "permission of this user successfully changed" << endl;
+                        cout << "do you want to change someone else permissions?" << endl;
+                        cout << "1 - yes" << endl;
+                        cout << "2 - no" << endl;
+                        cin >> i_c;
+                        if (i_c == 2)
+                        {
+                            stop_cycl = true;
+                        }
+                    }
+                    else
+                    {
+                        cout << "sorry, but you cant" << endl;
+                    }
+                }
+                break;
+            case 6:
+                stop_cycl = false;
+                while (!stop_cycl)
+                {
+                    cout << "which file?" << endl;
+                    iter__ = 0;
+                    int iter1__ = 0;
+                    int iter2__ = 0;
+                    p_file = file_comp_tab;
+                    do {
+                        iter__++;
+                        cout << iter__ << " - " << p_file->filename << ".txt" << endl;
+                        p_file = p_file->next; // переход к следующему узлу
+                    } while (p_file != NULL);
+                    cin >> iter__;
+                    perm_granted = check_perm_file(iter__, &cur_id_sess, 3, permition_matr);
+                    if (perm_granted)
+                    {
+                        p_auth_k = auth_data_list;
+                        do {
+                            int i_i = 0;
+                            i_i++;
+                            cout << i_i << " - " << p_auth_k->login << endl;
+                            p_auth_k = p_auth_k->next;
+                        } while (p_auth_k != NULL);
+                        cout << "which user?" << endl;
+                        cin >> iter1__;
+                        if (iter1__ != 1)
+                        {
+                            cout << "which permission want to grant?" << endl;
+                            cout << "1 - read" << endl;
+                            cout << "2 - read and write" << endl;
+                            cout << "3 - append permission" << endl;
+                            cin >> iter2__;
+                            permission_changer(permition_matr, iter1__, iter2__, iter__);
+                            cout << "permission of this user for chosen file successfully changed" << endl;
+                            stop_cycl = true;
+                        }
+                        else
+                        {
+                            cout << "sorry, but you cant" << endl;
+                        }
+                    }
+                    else
+                    {
+                        cout << "sorry, you dont have enough permissions" << endl;
+                    }
+                }
+                break;
+            case 9:
+                sess_authorised = false;
+                break;
+            }
+            
         }
-        kill_session_flag = true; //////temp!!!!!
+        //kill_session_flag = true; //////temp!!!!!
     }
     remove_all(auth_data_list);
     remove_all_file_comp(file_comp_tab);
     remove_all_Pmatr_outter(permition_matr);
+    free(filename);
     free(login_);
     free(password);
 }
